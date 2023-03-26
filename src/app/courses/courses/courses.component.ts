e6fe46accadc3881b0439cd0f1416d1e30d406db
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-03-26 17:02:36 -0300

    Adicionada Material Table de cursos

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
 
+
+export class CoursesComponent {
+  courses: Course[] = [
+    {
+      _id: "1",
+      category: "Back-end",
+      name: "SpringBoot"
+    },
+    {
+      _id: "2",
+      category: "Back-end",
+      name: "SpringMvc"
+    }
+  ];
+  displayedColumns = ['_id', 'category', 'name'];
+  constructor() {
+  }
 }
