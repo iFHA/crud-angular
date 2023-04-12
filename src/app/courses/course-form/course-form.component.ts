d9205d2b4e155ebfde5249498f96a03cdb6815a2
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-04-12 00:24:21 -0300

    utilizando snackbar em caso de erro

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CoursesService } from '../services/courses.service';
 
 @Component({
@@ -9,12 +10,20 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CourseFormComponent {
   form: FormGroup;
-  public constructor(private formBuilder:FormBuilder, private cs:CoursesService) {
+  public constructor(private formBuilder:FormBuilder, private cs:CoursesService, private snackBar: MatSnackBar) {
     this.form = this.formBuilder.group({
       name: [null],
       category: [null]
     })
   }
-  onSubmit() { this.cs.save(this.form.value).subscribe(result=>console.log(result)) }
+  onSubmit() {
+    this.cs.save(this.form.value).subscribe({
+      next: result => console.log(result),
+      error: () => this.onError()
+    });
+  }
   onCancel() {}
+  onError() {
+    this.snackBar.open('Erro', '', { duration: 3000 });
+  }
 }
