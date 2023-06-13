cb38748b2d82b2b55f6f74e6dda074d13fdecba6
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2023-06-13 19:06:18 -0300

    interface lesson

--- src/app/courses/guards/course.resolver.ts
+++ src/app/courses/guards/course.resolver.ts
@@ -17,6 +17,6 @@ export class CourseResolver implements Resolve<Course> {
     if(route.params && route.params['id']) {
       return this.cs.findById(route.params['id']);
     }
-    return of({ _id:"", category: "", name: "" });
+    return of({ _id:"", category: "", name: "", lessons: [] });
   }
 }
