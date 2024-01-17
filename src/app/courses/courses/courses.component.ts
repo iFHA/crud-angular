1b5e4f8f2db168eba7f91ffae08b298090aef258
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-16 22:00:41 -0300

    utilizando mat-table

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+import { Course } from '../model/course';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-
+  courses:Array<Course> = [
+    {
+      _id: '0',
+      name: 'curso1',
+      category: 'c'
+    }
+  ];
+  public displayedColumns: Array<keyof Course> = [
+    'name',
+    'category'
+  ];
 }
