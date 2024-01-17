8cfe848fd8d80b89e3dc531afa101a36cc92fcb0
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-16 23:19:04 -0300

    buscando dados de um json de forma assincrona com observable

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { Course } from '../model/course';
 import { CoursesService } from '../services/courses.service';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-  courses:Array<Course>;
+  courses$:Observable<Array<Course>>;
+  // courses: Array<Course>;
 
   constructor(private readonly courseService:CoursesService) {
-    this.courses = this.courseService.list();
+    this.courses$ = this.courseService.list();
   }
 
   public displayedColumns: Array<keyof Course> = [
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -1,30 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
-
+  private readonly API = '/assets/courses.json';
   constructor(private httpClient: HttpClient) { }
-  list(): Array<Course> {
-    return [
-      {
-        _id: '0',
-        name: 'curso1',
-        category: 'c'
-      },
-      {
-        _id: '1',
-        name: 'curso2',
-        category: 'c'
-      },
-      {
-        _id: '2',
-        name: 'curso3',
-        category: 'c'
-      }
-    ];
+  list(): Observable<Array<Course>> {
+    return this.httpClient.get<Array<Course>>(this.API);
   }
 }
