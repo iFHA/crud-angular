5f8e0436b2e5e00ee6d6af556b8ee0a68483b019
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-16 22:37:16 -0300

    refatoração centralizando imports do ng-material

---
+++ src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+
+
+
+@NgModule({
+  exports: [
+    MatToolbarModule,
+    MatTableModule,
+    MatCardModule
+  ]
+})
+export class AppMaterialModule { }
