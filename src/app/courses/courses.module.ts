ae960d68f972f79392d4fcd99db8b1cf49baafe5
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-23 19:59:01 -0300

    formulário para criar cursos

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -4,16 +4,19 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { CourseFormComponent } from './course-form/course-form.component';
 
 @NgModule({
   declarations: [
-    CoursesComponent
+    CoursesComponent,
+    CourseFormComponent
   ],
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    SharedModule
+    SharedModule,
+    ReactiveFormsModule
   ]
 })
 export class CoursesModule { }
