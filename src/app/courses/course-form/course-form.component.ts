513952089e00f26372330ca374d4b37788e04129
Fernando Henrique Alves <fernando.everywhere@gmail.com>
2024-01-23 23:23:44 -0300

    finalizado cadastro via post

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,8 +1,9 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { CoursesService } from '../services/courses.service';
-import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -15,7 +16,8 @@ export class CourseFormComponent {
     public formBuilder:FormBuilder,
     private coursesService:CoursesService,
     private router:Router,
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private location: Location
     ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -24,17 +26,24 @@ export class CourseFormComponent {
   }
   onSubmit(){
     this.coursesService.save(this.form.value).subscribe({
-      next: console.log,
+      next: course => this.onSuccess(),
       error: error => this.onError()
     });
   }
   onCancel(){
     this.router.navigate(['courses']);
   }
-  onError() {
+  private onSuccess() {
+    this.openSnackBar("Curso salvo com sucesso");
+    this.back();
+  }
+  private onError() {
     this.openSnackBar("Erro ao salvar curso");
   }
-  openSnackBar(message: string) {
+  private openSnackBar(message: string) {
     this.snackBar.open(message, '', {duration:3000});
+  }
+  private back() {
+    this.location.back();
   }
 }
